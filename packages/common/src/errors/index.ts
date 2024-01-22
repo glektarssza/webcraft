babf19a6ec8262d0883cea7ecefc3737827df4a8
G'lek Tarssza <glek@glektarssza.com>
2024-01-22 11:10:20 -0700

    Doing some cleanup.

--- packages/common/src/errors/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './baseError';
-export * from './argumentError';
-export * from './argumentNilError';
-export * from './argumentRangeError';
-export * from './operationError';
-export * from './invalidStateError';
-export * from './objectDisposedError';
--- packages/common/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * as errors from './errors';
-export * from './disposable';
