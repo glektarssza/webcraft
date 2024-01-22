37fd864ed5697a4241c3ecae2a0d236f58d5bbff
G'lek Tarssza <glek@glektarssza.com>
2024-01-22 12:42:36 -0700

    Forgot to export argument range error.

--- packages/common/src/errors/index.ts
+++ packages/common/src/errors/index.ts
@@ -1,3 +1,4 @@
 export {BaseError} from './baseError';
 export {ArgumentError} from './argumentError';
 export {ArgumentNilError} from './argumentNilError';
+export {ArgumentRangeError} from './argumentRangeError';
