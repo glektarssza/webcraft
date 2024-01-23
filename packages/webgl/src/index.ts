44947dac3d420231e152bc1895f87d2134099085
G'lek Tarssza <glek@glektarssza.com>
2024-01-23 12:10:56 -0700

    Added buffer usage hint enum.

--- packages/webgl/src/index.ts
+++ packages/webgl/src/index.ts
@@ -1,4 +1,5 @@
 export {ErrorCode} from './errorCode';
 export {BufferTarget} from './bufferTarget';
+export {BufferUsageHint} from './bufferUsageHint';
 export {Context} from './context';
 export {Resource, ValidWebGLResources} from './resource';
