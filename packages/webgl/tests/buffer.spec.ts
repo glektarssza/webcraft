6cb1ff67406294b913ddd67e1d9207f638532894
G'lek Tarssza <glek@glektarssza.com>
2024-01-24 08:30:15 -0700

    Some more buffer tests.

--- packages/webgl/tests/buffer.spec.ts
+++ packages/webgl/tests/buffer.spec.ts
@@ -10,6 +10,7 @@ import {Buffer} from '@src/buffer';
 import {BufferTarget} from '@src/bufferTarget';
 import {BufferUsageHint} from '@src/bufferUsageHint';
 import {Context} from '@src/context';
+import {OperationError} from 'webcraft-common';
 
 chai.use(sinonChai);
 
@@ -130,6 +131,32 @@ describe('module:webcraft-webgl', () => {
             });
         });
         describe('.usageHint', () => {
+            it('should return `null` if the instance is disposed', () => {
+                //-- Given
+                const target = faker.helpers.arrayElement(bufferTargets);
+                nativeContext.createBuffer.returns(native);
+                const buffer = new Buffer(context, target);
+                Reflect.set(buffer, '_disposed', true);
+
+                //-- When
+                const r = buffer.usageHint;
+
+                //-- Then
+                expect(r).to.be.null;
+            });
+            it('should return `null` if the instance does not wrap a valid WebGL buffer', () => {
+                //-- Given
+                const target = faker.helpers.arrayElement(bufferTargets);
+                nativeContext.createBuffer.returns(native);
+                const buffer = new Buffer(context, target);
+                Reflect.set(buffer, '_native', null);
+
+                //-- When
+                const r = buffer.usageHint;
+
+                //-- Then
+                expect(r).to.be.null;
+            });
             it('should be set to `null` if the instance has not been configured yet', () => {
                 //-- Given
                 const target = faker.helpers.arrayElement(bufferTargets);
@@ -170,24 +197,151 @@ describe('module:webcraft-webgl', () => {
             });
         });
         describe('.isBound', () => {
-            it(
-                'should be set to `true` if the instance is bound to its target'
-            );
-            it(
-                'should be set to `false` if the instance is not bound to its target'
-            );
+            it('should return `false` if the instance is disposed', () => {
+                //-- Given
+                const target = faker.helpers.arrayElement(bufferTargets);
+                nativeContext.createBuffer.returns(native);
+                const buffer = new Buffer(context, target);
+                Reflect.set(buffer, '_disposed', true);
+
+                //-- When
+                const r = buffer.isBound;
+
+                //-- Then
+                expect(r).to.be.false;
+                expect(nativeContext.getParameter).to.not.have.been.called;
+            });
+            it('should return `false` if the instance does not wrap a valid WebGL buffer', () => {
+                //-- Given
+                const target = faker.helpers.arrayElement(bufferTargets);
+                nativeContext.createBuffer.returns(native);
+                const buffer = new Buffer(context, target);
+                Reflect.set(buffer, '_native', null);
+
+                //-- When
+                const r = buffer.isBound;
+
+                //-- Then
+                expect(r).to.be.false;
+                expect(nativeContext.getParameter).to.not.have.been.called;
+            });
+            it('should be set to `true` if the instance is bound to its target', () => {
+                //-- Given
+                const [target, bindingParam] =
+                    faker.helpers.arrayElement(bufferParams);
+                nativeContext.createBuffer.returns(native);
+                const buffer = new Buffer(context, target);
+                nativeContext.getParameter
+                    .withArgs(bindingParam)
+                    .returns(native);
+
+                //-- When
+                const r = buffer.isBound;
+
+                //-- Then
+                expect(r).to.be.true;
+                expect(
+                    nativeContext.getParameter
+                ).to.have.been.calledOnceWithExactly(bindingParam);
+            });
+            it('should be set to `false` if the instance is not bound to its target', () => {
+                //-- Given
+                const [target, bindingParam] =
+                    faker.helpers.arrayElement(bufferParams);
+                nativeContext.createBuffer.returns(native);
+                const buffer = new Buffer(context, target);
+                nativeContext.getParameter.withArgs(bindingParam).returns(null);
+
+                //-- When
+                const r = buffer.isBound;
+
+                //-- Then
+                expect(r).to.be.false;
+                expect(
+                    nativeContext.getParameter
+                ).to.have.been.calledOnceWithExactly(bindingParam);
+            });
         });
         describe('.constructor()', () => {
-            it('should pass the `context` parameter to the base class');
-            it(
-                'should set the `native` property to the newly created native WebGL buffer'
-            );
-            it(
-                'should throw an `OperationError` if the native WebGL buffer fails to be created'
-            );
-            it('should set the `target` property to the given value');
-            it('should set the `allocatedSize` property to `null`');
-            it('should set the `usageHint` property to `null`');
+            it('should pass the `context` parameter to the base class', () => {
+                //-- Given
+                const target = faker.helpers.arrayElement(bufferTargets);
+                nativeContext.createBuffer.returns(native);
+
+                //-- When
+                const r = new Buffer(context, target);
+
+                //-- Then
+                expect(r.context).to.equal(context);
+            });
+            it('should set the `native` property to the newly created native WebGL buffer', () => {
+                //-- Given
+                const target = faker.helpers.arrayElement(bufferTargets);
+                nativeContext.createBuffer.returns(native);
+
+                //-- When
+                const r = new Buffer(context, target);
+
+                //-- Then
+                expect(r.native).to.equal(native);
+            });
+            it('should throw an `OperationError` if the native WebGL buffer fails to be created', () => {
+                //-- Given
+                const target = faker.helpers.arrayElement(bufferTargets);
+                nativeContext.createBuffer.returns(null);
+
+                //-- When
+                try {
+                    new Buffer(context, target);
+                } catch (ex) {
+                    //-- Then
+                    expect(ex).to.be.an.instanceOf(OperationError);
+                    expect(ex)
+                        .to.have.a.property('operationName')
+                        .which.equals('createBuffer');
+                    expect(ex)
+                        .to.have.a.property('message')
+                        .which.equals(
+                            'Failed to create native WebGL buffer resource'
+                        );
+                    return;
+                }
+
+                expect.fail('Function did not throw when it should have');
+            });
+            it('should set the `target` property to the given value', () => {
+                //-- Given
+                const target = faker.helpers.arrayElement(bufferTargets);
+                nativeContext.createBuffer.returns(native);
+
+                //-- When
+                const r = new Buffer(context, target);
+
+                //-- Then
+                expect(r.target).to.equal(target);
+            });
+            it('should set the `allocatedSize` property to `null`', () => {
+                //-- Given
+                const target = faker.helpers.arrayElement(bufferTargets);
+                nativeContext.createBuffer.returns(native);
+
+                //-- When
+                const r = new Buffer(context, target);
+
+                //-- Then
+                expect(r.allocatedSize).to.be.null;
+            });
+            it('should set the `usageHint` property to `null`', () => {
+                //-- Given
+                const target = faker.helpers.arrayElement(bufferTargets);
+                nativeContext.createBuffer.returns(native);
+
+                //-- When
+                const r = new Buffer(context, target);
+
+                //-- Then
+                expect(r.usageHint).to.be.null;
+            });
         });
         describe('.bind()', () => {
             it(
