3bd9ff1808444df521232398b35be3536cd63c76
G'lek Tarssza <glek@glektarssza.com>
2024-01-24 08:18:18 -0700

    Finished shader tests.

--- packages/webgl/tests/shader.spec.ts
+++ packages/webgl/tests/shader.spec.ts
@@ -8,7 +8,7 @@ import {Faker, en, en_CA, en_US, base} from '@faker-js/faker';
 import {Shader} from '@src/shader';
 import {ShaderType} from '@src/shaderType';
 import {Context} from '@src/context';
-import {OperationError} from 'webcraft-common';
+import {DisposedError, OperationError, StateError} from 'webcraft-common';
 
 chai.use(sinonChai);
 
@@ -49,6 +49,34 @@ describe('module:webcraft-webgl', () => {
             });
         });
         describe('.sourceCode', () => {
+            it('should return `null` if the instance is disposed', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                Reflect.set(shader, '_disposed', true);
+
+                //-- When
+                const r = shader.sourceCode;
+
+                //-- Then
+                expect(r).to.be.null;
+                expect(nativeContext.getShaderSource).to.not.have.been.called;
+            });
+            it('should return `null` if the instance does not wrap a valid WebGL shader', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                Reflect.set(shader, '_native', null);
+
+                //-- When
+                const r = shader.sourceCode;
+
+                //-- Then
+                expect(r).to.be.null;
+                expect(nativeContext.getShaderSource).to.not.have.been.called;
+            });
             it('should be set the `null` if no source code has been uploaded to the instance yet', () => {
                 //-- Given
                 const type = faker.helpers.arrayElement(shaderTypes);
@@ -80,6 +108,34 @@ describe('module:webcraft-webgl', () => {
             });
         });
         describe('.infoLog', () => {
+            it('should return `null` if the instance is disposed', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                Reflect.set(shader, '_disposed', true);
+
+                //-- When
+                const r = shader.infoLog;
+
+                //-- Then
+                expect(r).to.be.null;
+                expect(nativeContext.getShaderInfoLog).to.not.have.been.called;
+            });
+            it('should return `null` if the instance does not wrap a valid WebGL shader', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                Reflect.set(shader, '_native', null);
+
+                //-- When
+                const r = shader.infoLog;
+
+                //-- Then
+                expect(r).to.be.null;
+                expect(nativeContext.getShaderInfoLog).to.not.have.been.called;
+            });
             it('should be set the `null` if the instance has not been compiled yet', () => {
                 //-- Given
                 const type = faker.helpers.arrayElement(shaderTypes);
@@ -111,6 +167,36 @@ describe('module:webcraft-webgl', () => {
             });
         });
         describe('.isCompiled', () => {
+            it('should return `false` if the instance is disposed', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                Reflect.set(shader, '_disposed', true);
+
+                //-- When
+                const r = shader.isCompiled;
+
+                //-- Then
+                expect(r).to.be.false;
+                expect(nativeContext.getShaderParameter).to.not.have.been
+                    .called;
+            });
+            it('should return `false` if the instance does not wrap a valid WebGL shader', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                Reflect.set(shader, '_native', null);
+
+                //-- When
+                const r = shader.isCompiled;
+
+                //-- Then
+                expect(r).to.be.false;
+                expect(nativeContext.getShaderParameter).to.not.have.been
+                    .called;
+            });
             it('should be set to `false` if the instance has not been compiled yet', () => {
                 //-- Given
                 const type = faker.helpers.arrayElement(shaderTypes);
@@ -177,13 +263,13 @@ describe('module:webcraft-webgl', () => {
                     //-- Then
                     expect(ex).to.be.an.instanceOf(OperationError);
                     expect(ex)
-                        .to.have.property('message')
+                        .to.have.a.property('operationName')
+                        .which.equals('createShader');
+                    expect(ex)
+                        .to.have.a.property('message')
                         .which.equals(
                             'Failed to create native WebGL shader resource'
                         );
-                    expect(ex)
-                        .to.have.property('operationName')
-                        .which.equals('createShader');
                     return;
                 }
 
@@ -202,27 +288,297 @@ describe('module:webcraft-webgl', () => {
             });
         });
         describe('.uploadSourceCode()', () => {
-            it(
-                'should throw a `DisposedError` if the instance has been disposed'
-            );
-            it(
-                'should throw a `StateError` if the instance does not wrap a valid WebGL shader'
-            );
-            it('should upload the given source code to the instance');
-            it('should throw an `OperationError` if a WebGL error occurs');
+            it('should throw a `DisposedError` if the instance has been disposed', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                Reflect.set(shader, '_disposed', true);
+                const sourceCode = faker.lorem.paragraph();
+
+                //-- When
+                try {
+                    shader.uploadSourceCode(sourceCode);
+                } catch (ex) {
+                    //-- Then
+                    expect(ex).to.be.an.instanceOf(DisposedError);
+                    expect(ex)
+                        .to.have.a.property('objectType')
+                        .which.equals('Shader');
+                    expect(ex)
+                        .to.have.a.property('operationName')
+                        .which.equals('uploadSourceCode');
+                    expect(ex)
+                        .to.have.a.property('message')
+                        .which.equals(
+                            'Cannot upload source code to a disposed WebGL shader'
+                        );
+                    return;
+                }
+
+                expect.fail('Function did not throw when it should have');
+            });
+            it('should throw a `StateError` if the instance does not wrap a valid WebGL shader', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                Reflect.set(shader, '_native', null);
+                const sourceCode = faker.lorem.paragraph();
+
+                //-- When
+                try {
+                    shader.uploadSourceCode(sourceCode);
+                } catch (ex) {
+                    //-- Then
+                    expect(ex).to.be.an.instanceOf(StateError);
+                    expect(ex)
+                        .to.have.a.property('objectType')
+                        .which.equals('Shader');
+                    expect(ex)
+                        .to.have.a.property('operationName')
+                        .which.equals('uploadSourceCode');
+                    expect(ex)
+                        .to.have.a.property('message')
+                        .which.equals(
+                            'Cannot upload source code to an invalid WebGL shader'
+                        );
+                    return;
+                }
+
+                expect.fail('Function did not throw when it should have');
+            });
+            it('should upload the given source code to the instance', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                const sourceCode = faker.lorem.paragraph();
+                nativeContext.shaderSource
+                    .withArgs(native, sourceCode)
+                    .returns();
+                nativeContext.getError.returns(WebGLRenderingContext.NO_ERROR);
+
+                //-- When
+                shader.uploadSourceCode(sourceCode);
+
+                //-- Then
+                expect(
+                    nativeContext.shaderSource
+                ).to.have.been.calledOnceWithExactly(native, sourceCode);
+            });
+            it('should throw an `OperationError` if a WebGL error occurs', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                const sourceCode = faker.lorem.paragraph();
+                nativeContext.shaderSource
+                    .withArgs(native, sourceCode)
+                    .returns();
+                nativeContext.getError.returns(
+                    WebGLRenderingContext.INVALID_VALUE
+                );
+
+                //-- When
+                try {
+                    shader.uploadSourceCode(sourceCode);
+                } catch (ex) {
+                    //-- Then
+                    expect(ex).to.be.an.instanceOf(OperationError);
+                    expect(ex)
+                        .to.have.a.property('operationName')
+                        .which.equals('uploadSourceCode');
+                    expect(ex)
+                        .to.have.a.property('message')
+                        .which.equals(
+                            'An error occurred while uploading source code to a WebGL shader'
+                        );
+                    return;
+                }
+
+                expect.fail('Function did not throw when it should have');
+            });
         });
         describe('.compile()', () => {
-            it(
-                'should throw a `DisposedError` if the instance has been disposed'
-            );
-            it(
-                'should throw a `StateError` if the instance does not wrap a valid WebGL shader'
-            );
-            it(
-                'should throw a `StateError` if the instance does not have any source code uploaded to it yet'
-            );
-            it('should compile the instance');
-            it('should throw an `OperationError` if a WebGL error occurs');
+            it('should throw a `DisposedError` if the instance has been disposed', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                Reflect.set(shader, '_disposed', true);
+
+                //-- When
+                try {
+                    shader.compile();
+                } catch (ex) {
+                    //-- Then
+                    expect(ex).to.be.an.instanceOf(DisposedError);
+                    expect(ex)
+                        .to.have.a.property('objectType')
+                        .which.equals('Shader');
+                    expect(ex)
+                        .to.have.a.property('operationName')
+                        .which.equals('compile');
+                    expect(ex)
+                        .to.have.a.property('message')
+                        .which.equals('Cannot compile a disposed WebGL shader');
+                    return;
+                }
+
+                expect.fail('Function did not throw when it should have');
+            });
+            it('should throw a `StateError` if the instance does not wrap a valid WebGL shader', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                Reflect.set(shader, '_native', null);
+
+                //-- When
+                try {
+                    shader.compile();
+                } catch (ex) {
+                    //-- Then
+                    expect(ex).to.be.an.instanceOf(StateError);
+                    expect(ex)
+                        .to.have.a.property('objectType')
+                        .which.equals('Shader');
+                    expect(ex)
+                        .to.have.a.property('operationName')
+                        .which.equals('compile');
+                    expect(ex)
+                        .to.have.a.property('message')
+                        .which.equals('Cannot compile an invalid WebGL shader');
+                    return;
+                }
+
+                expect.fail('Function did not throw when it should have');
+            });
+            it('should throw a `StateError` if the instance does not have any source code uploaded to it yet', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                nativeContext.getShaderSource.withArgs(native).returns(null);
+
+                //-- When
+                try {
+                    shader.compile();
+                } catch (ex) {
+                    //-- Then
+                    expect(ex).to.be.an.instanceOf(StateError);
+                    expect(ex)
+                        .to.have.a.property('objectType')
+                        .which.equals('Shader');
+                    expect(ex)
+                        .to.have.a.property('operationName')
+                        .which.equals('compile');
+                    expect(ex)
+                        .to.have.a.property('message')
+                        .which.equals(
+                            'Cannot compile a WebGL shader with no source code'
+                        );
+                    return;
+                }
+
+                expect.fail('Function did not throw when it should have');
+            });
+            it('should compile the instance', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                const sourceCode = faker.lorem.paragraph();
+                nativeContext.getShaderSource
+                    .withArgs(native)
+                    .returns(sourceCode);
+                nativeContext.compileShader.withArgs(native).returns();
+                nativeContext.getError.returns(WebGLRenderingContext.NO_ERROR);
+
+                //-- When
+                shader.compile();
+
+                //-- Then
+                expect(
+                    nativeContext.compileShader
+                ).to.have.been.calledOnceWithExactly(native);
+            });
+            it('should throw an `OperationError` if a WebGL error occurs', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                const sourceCode = faker.lorem.paragraph();
+                nativeContext.getShaderSource
+                    .withArgs(native)
+                    .returns(sourceCode);
+                nativeContext.compileShader.withArgs(native).returns();
+                nativeContext.getError.returns(
+                    WebGLRenderingContext.INVALID_OPERATION
+                );
+
+                //-- When
+                try {
+                    shader.compile();
+                } catch (ex) {
+                    //-- Then
+                    expect(ex).to.be.an.instanceOf(OperationError);
+                    expect(ex)
+                        .to.have.a.property('operationName')
+                        .which.equals('compile');
+                    expect(ex)
+                        .to.have.a.property('message')
+                        .which.equals(
+                            'An error occurred while compiling a WebGL shader'
+                        );
+                    return;
+                }
+
+                expect.fail('Function did not throw when it should have');
+            });
+        });
+        describe('.dispose()', () => {
+            it('should do nothing if the instance is already disposed', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                Reflect.set(shader, '_disposed', true);
+
+                //-- When
+                shader.dispose();
+
+                //-- Then
+                expect(nativeContext.deleteShader).to.not.have.been.called;
+            });
+            it('should do nothing if the instance does not wrap a valid WebGL shader object', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+                Reflect.set(shader, '_native', null);
+
+                //-- When
+                shader.dispose();
+
+                //-- Then
+                expect(nativeContext.deleteShader).to.not.have.been.called;
+            });
+            it('should delete the wrapped WebGL shader object', () => {
+                //-- Given
+                const type = faker.helpers.arrayElement(shaderTypes);
+                nativeContext.createShader.withArgs(type).returns(native);
+                const shader = new Shader(context, type);
+
+                //-- When
+                shader.dispose();
+
+                //-- Then
+                expect(
+                    nativeContext.deleteShader
+                ).to.have.been.calledOnceWithExactly(native);
+            });
         });
     });
 });
