642ba2dba602cf91fe90ee29936e57012c423646
G'lek Tarssza <glek@glektarssza.com>
2024-01-24 12:23:47 -0700

    Fleshed out context tests a bit.

--- packages/webgl/tests/context.spec.ts
+++ packages/webgl/tests/context.spec.ts
@@ -1,16 +1,67 @@
 //-- NPM Packages
 import chai, {expect} from 'chai';
-import {createStubInstance} from 'sinon';
+import {createStubInstance, stub} from 'sinon';
 import sinonChai from 'sinon-chai';
 
 //-- Project Code
 import {Context} from '@src/context';
 import {ErrorCode} from '@src/errorCode';
+import {Resource} from '../src/resource';
 
 chai.use(sinonChai);
 
 describe('module:webcraft-webgl', () => {
     describe('class:Context', () => {
+        describe('.resources', () => {
+            it('should be an empty list by default', () => {
+                //-- Given
+                const native = createStubInstance(WebGLRenderingContext);
+                const context = new Context(native);
+
+                //-- When
+                const r = context.resources;
+
+                //-- Then
+                expect(r).to.be.empty;
+            });
+            it('should contain any managed resources', () => {
+                //-- Given
+                const native = createStubInstance(WebGLRenderingContext);
+                const context = new Context(native);
+                class TestResource extends Resource<WebGLBuffer> {
+                    public constructor(context: Context) {
+                        super(context);
+                    }
+                }
+                const resource = new TestResource(context);
+                Reflect.set(context, '_resources', [resource]);
+
+                //-- When
+                const r = context.resources;
+
+                //-- Then
+                expect(r).to.include(resource);
+            });
+            it('should be a shallow clone', () => {
+                //-- Given
+                const native = createStubInstance(WebGLRenderingContext);
+                const context = new Context(native);
+                class TestResource extends Resource<WebGLBuffer> {
+                    public constructor(context: Context) {
+                        super(context);
+                    }
+                }
+                const resource = new TestResource(context);
+                const resources = [resource];
+                Reflect.set(context, '_resources', resources);
+
+                //-- When
+                const r = context.resources;
+
+                //-- Then
+                expect(r).to.not.equal(resources);
+            });
+        });
         describe('.isDisposed', () => {
             it('should be `true` if the instance has been disposed', () => {
                 //-- Given
@@ -98,7 +149,63 @@ describe('module:webcraft-webgl', () => {
             });
         });
         describe('.dispose()', () => {
-            it('should do nothing if the instance has already been disposed');
+            it('should do nothing if the instance has already been disposed', () => {
+                //-- Given
+                const native = createStubInstance(WebGLRenderingContext);
+                const context = new Context(native);
+                class TestResource extends Resource<WebGLBuffer> {
+                    public constructor(context: Context) {
+                        super(context);
+                    }
+                }
+                const resource = new TestResource(context);
+                const disposeStub = stub(resource, 'dispose');
+                Reflect.set(context, '_resources', [resource]);
+                Reflect.set(context, '_disposed', true);
+
+                //-- When
+                context.dispose();
+
+                //-- Then
+                expect(disposeStub).to.not.have.been.called;
+            });
+            it('should dispose of all managed resource objects', () => {
+                //-- Given
+                const native = createStubInstance(WebGLRenderingContext);
+                const context = new Context(native);
+                class TestResource extends Resource<WebGLBuffer> {
+                    public constructor(context: Context) {
+                        super(context);
+                    }
+                }
+                const resource = new TestResource(context);
+                const disposeStub = stub(resource, 'dispose');
+                Reflect.set(context, '_resources', [resource]);
+
+                //-- When
+                context.dispose();
+
+                //-- Then
+                expect(disposeStub).to.have.been.calledOnce;
+            });
+            it('should set the `resources` property to an empty list', () => {
+                //-- Given
+                const native = createStubInstance(WebGLRenderingContext);
+                const context = new Context(native);
+                class TestResource extends Resource<WebGLBuffer> {
+                    public constructor(context: Context) {
+                        super(context);
+                    }
+                }
+                const resource = new TestResource(context);
+                Reflect.set(context, '_resources', [resource]);
+
+                //-- When
+                context.dispose();
+
+                //-- Then
+                expect(context.resources).to.be.empty;
+            });
             it('should set the `isDisposed` property to `true`', () => {
                 //-- Given
                 const native = createStubInstance(WebGLRenderingContext);
