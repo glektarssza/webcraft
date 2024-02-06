336ea7026752a35f9a76c5ff7b3a683d424cb6cb
G'lek Tarssza <glek@glektarssza.com>
2024-02-05 21:22:01 -0700

    Removed sinon-chai from package template.

--- templates/packages/tests/index.spec.ts
+++ templates/packages/tests/index.spec.ts
@@ -1,14 +1,11 @@
 //-- NPM Packages
-import chai, {expect} from 'chai';
-import sinonChai from 'sinon-chai';
+import {expect} from 'chai';
 import {Faker, en, en_CA, en_US, base} from '@faker-js/faker';
 
 //-- Project Code
 import {getTestModule} from '@src/index';
 
 const module = getTestModule();
-
-chai.use(sinonChai);
 
 /**
  * The fake data generator.
