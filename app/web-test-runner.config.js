ffc9fe7db16dc12a5737de12b35512113a1922b8
G'lek Tarssza <glek@glektarssza.com>
2024-03-25 11:51:21 -0600

    Fixed up coverage config.

--- app/web-test-runner.config.js
+++ app/web-test-runner.config.js
@@ -6,7 +6,7 @@ const {esbuildPlugin} = require('@web/dev-server-esbuild');
 const {summaryReporter} = require('@web/test-runner');
 
 module.exports = {
-    files: ['./tests/**/*.spec.ts'],
+    files: ['./tests/**/*.spec.ts', '!**/node_modules/**/*'],
     puppeteer: true,
     nodeResolve: true,
     plugins: [
@@ -15,5 +15,11 @@ module.exports = {
             tsconfig: path.resolve(__dirname, './tests/tsconfig.json')
         })
     ],
-    reporters: [summaryReporter()]
+    reporters: [summaryReporter()],
+    coverageConfig: {
+        report: true,
+        reportDir: 'coverage',
+        include: ['./src/ts/**/*.ts'],
+        reporters: ['text', 'html']
+    }
 };
