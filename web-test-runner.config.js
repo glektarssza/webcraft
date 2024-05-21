f572736652537602a6537cb2b66b732f96d97fae
G'lek Tarssza <glek@glektarssza.com>
2024-05-21 07:56:30 -0600

    Set environment so Vite knows if we're unit testing.

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -5,6 +5,9 @@ import {
 } from '@remcovaes/web-test-runner-vite-plugin';
 import {puppeteerLauncher} from '@web/test-runner-puppeteer';
 
+// HACK: Let Vite know we're running unit tests
+process.env['VITE_UNIT_TESTING'] = 'true';
+
 /**
  * @type {import('@web/test-runner').TestRunnerConfig}
  */
