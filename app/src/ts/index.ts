2a9ce4b29a2a2ccde2f97bb8fddd192e1d109add
G'lek Tarssza <glek@glektarssza.com>
2024-06-02 21:16:37 -0600

    Setup main.

--- app/src/ts/index.ts
+++ app/src/ts/index.ts
@@ -1,18 +1,22 @@
 //-- Project Code
-import {getMessage} from '@glektarssza/webcraft-common';
+import {utils} from '@glektarssza/webcraft-common';
 
-if (document.readyState !== 'complete') {
-    document.addEventListener('readystatechange', () => {
-        if (document.readyState === 'complete') {
-            const e = document.body.children[0];
-            if (e !== undefined) {
-                e.textContent = getMessage();
-            }
-        }
-    });
-} else {
-    const e = document.body.children[0];
-    if (e !== undefined) {
-        e.textContent = getMessage();
-    }
+/**
+ * The program entry point.
+ *
+ * @returns A promise that resolves when the program has started or rejects if
+ * an error occurs.
+ */
+async function main(): Promise<void> {
+    await utils.dom.waitForDOMReady();
+    // TODO
 }
+
+main()
+    .then((): void => {
+        console.log('Application started normally');
+    })
+    .catch((err: Error): void => {
+        console.error('Fatal error:');
+        console.error(err);
+    });
