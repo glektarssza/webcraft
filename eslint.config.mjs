ba52e423ef85587092946ea818a44efd95aed3a3
G'lek Tarssza <glek@glektarssza.com>
2024-06-04 16:46:12 -0600

    Moved to latest eslint and typescript-eslint.

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+//-- NPM Packages
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
+
+export default tseslint.config(
+    {
+        extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked, prettierPlugin],
+        files: ['**/*.ts'],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname
+            }
+        }
+    },
+    {
+        files: ['**/tests/**/*.ts'],
+        rules: {
+            '@typescript-eslint/no-unused-expressions': 'off'
+        }
+    }
+);
