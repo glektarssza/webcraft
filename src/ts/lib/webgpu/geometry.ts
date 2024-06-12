3da6897c60c8ab57d0b6560224fb100bf38c0a67
G'lek Tarssza <glek@glektarssza.com>
2024-06-12 07:59:54 -0600

    Refactored geometry stuff a bit.

--- src/ts/lib/webgpu/geometry.ts
+++ src/ts/lib/webgpu/geometry.ts
@@ -1,79 +1,90 @@
+//-- NPM Packages
+import flatten from 'lodash/flatten';
+
 //-- Project Code
 import type {Vertex} from './types';
-
-/**
- * An object which describes some 3D shape.
- */
-export interface Geometry {
-    /**
-     * An array of vertex positions that compose the geometry.
-     */
-    vertices: Vertex[];
-}
 
 /**
  * A module which provides various geometry-related functionality.
  */
 const m = {
-    createCubeGeometry(width = 1, height = 1, depth = 1): Geometry {
-        return {
-            // prettier-ignore
-            vertices: [
-                //-- Back Face
-                [0,     0,      0],
-                [width, 0,      0],
-                [width, height, 0],
+    /**
+     * Convert some geometry to a typed array.
+     *
+     * @param geometry - The geometry to convert.
+     *
+     * @returns The geometry as a typed array.
+     */
+    geometryToTypedArray(geometry: Vertex[]): Float32Array {
+        return new Float32Array(flatten(geometry));
+    },
 
-                [0,     0,      0],
-                [0,     height, 0],
-                [width, height, 0],
+    /**
+     * Create the geometry for a cube.
+     *
+     * @param width - The width of the cube, in world units.
+     * @param height - The height of the cube, in world units.
+     * @param depth - The depth of the cube, in world units.
+     *
+     * @returns The geometry for a cube.
+     */
+    createCubeGeometry(width = 1, height = 1, depth = 1): Vertex[] {
+        // prettier-ignore
+        return [
+            //-- Back Face
+            [0,     0,      0],
+            [width, 0,      0],
+            [width, height, 0],
 
-                //-- Front Face
-                [0,     0,      depth],
-                [width, 0,      depth],
-                [width, height, depth],
+            [0,     0,      0],
+            [0,     height, 0],
+            [width, height, 0],
 
-                [0,     0,      depth],
-                [0,     height, depth],
-                [width, height, depth],
+            //-- Front Face
+            [0,     0,      depth],
+            [width, 0,      depth],
+            [width, height, depth],
 
-                //-- Top Face
-                [0,     height, 0],
-                [0,     height, depth],
-                [width, height, depth],
+            [0,     0,      depth],
+            [0,     height, depth],
+            [width, height, depth],
 
-                [0,     height, 0],
-                [width, height, 0],
-                [width, height, depth],
+            //-- Top Face
+            [0,     height, 0],
+            [0,     height, depth],
+            [width, height, depth],
 
-                //-- Bottom Face
-                [0,     0, 0],
-                [0,     0, depth],
-                [width, 0, depth],
+            [0,     height, 0],
+            [width, height, 0],
+            [width, height, depth],
 
-                [0,     0, 0],
-                [width, 0, 0],
-                [width, 0, depth],
+            //-- Bottom Face
+            [0,     0, 0],
+            [0,     0, depth],
+            [width, 0, depth],
 
-                //-- Left Face
-                [0, 0,      0],
-                [0, 0,      depth],
-                [0, height, depth],
+            [0,     0, 0],
+            [width, 0, 0],
+            [width, 0, depth],
 
-                [0, 0,      0],
-                [0, height, 0],
-                [0, height, depth],
+            //-- Left Face
+            [0, 0,      0],
+            [0, 0,      depth],
+            [0, height, depth],
 
-                //-- Right Face
-                [width, 0,      0],
-                [width, 0,      depth],
-                [width, height, depth],
+            [0, 0,      0],
+            [0, height, 0],
+            [0, height, depth],
 
-                [width, 0,      0],
-                [width, height, 0],
-                [width, height, depth]
-            ] as Vertex[]
-        };
+            //-- Right Face
+            [width, 0,      0],
+            [width, 0,      depth],
+            [width, height, depth],
+
+            [width, 0,      0],
+            [width, height, 0],
+            [width, height, depth]
+        ] as Vertex[];
     }
 };
 
