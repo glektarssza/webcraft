d817f04f8fa9ee2f4a8ecb28e77639bdcb982f00
G'lek Tarssza <glek@glektarssza.com>
2024-06-28 12:14:26 -0600

    Moved distinct type to package root.

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,3 +1,8 @@
-//-- Project Code
-export * from './utilTypes';
-export * from './dom';
+/**
+ * A type which represents a version of an existing type that is considered
+ * distinct from other instances of that type while still being interoperable
+ * with them.
+ *
+ * @typeParam T - The underlying type to make the distinct type out of.
+ */
+export type Distinct<T> = T & {readonly __TYPE__: unique symbol};
