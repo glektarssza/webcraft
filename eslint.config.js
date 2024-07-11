a914d00228492979f672de85bfde24918f748a8c
G'lek Tarssza <glek@glektarssza.com>
2024-07-11 11:20:58 -0600

    Configured ESLint.

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+// @ts-check
+
+//-- NPM Packages
+import eslint from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    {
+        ignores: ['*.js', '*.cjs', '*.mjs']
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname
+            }
+        }
+    },
+    prettierConfig
+);
