bf1a5a278667d1cdd5afb3a8487d230c1a26120f
G'lek Tarssza <glek@glektarssza.com>
2024-07-11 11:49:18 -0600

    Added Vite config to main project.

---
+++ app/vite.config.ts
@@ -0,0 +1,54 @@
+/// <reference types="vitest" />
+
+//-- NodeJS
+import os from 'node:os';
+import path from 'node:path';
+
+//-- NPM Packages
+import replacePlugin from '@rollup/plugin-replace';
+import {defineConfig} from 'vite';
+
+/**
+ * The ViteJS configuration.
+ */
+const config = defineConfig(({mode}) => {
+    return {
+        mode,
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        build: {
+            outDir: path.resolve(import.meta.dirname, './dist/'),
+            minify: mode !== 'development',
+            emptyOutDir: true
+        },
+        test: {
+            alias: {
+                '@src': path.resolve(import.meta.dirname, './src/ts/')
+            },
+            coverage: {
+                all: true,
+                reporter: ['text', 'html']
+            },
+            mockReset: true,
+            clearMocks: true,
+            unstubGlobals: true,
+            unstubEnvs: true,
+            dir: './tests/',
+            name: 'Webcraft',
+            maxConcurrency: Math.max(Math.floor(os.cpus().length / 2), 1),
+            reporters: 'default',
+            passWithNoTests: true
+        },
+        plugins: [
+            replacePlugin({
+                preventAssignment: true,
+                values: {
+                    FAKER_SEED: JSON.stringify(process.env['FAKER_SEED'])
+                }
+            })
+        ]
+    };
+});
+
+export default config;
