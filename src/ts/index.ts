884e09465095c62b189b62491da1ff11d62a2238
G'lek Tarssza <glek@glektarssza.com>
2024-07-16 11:52:39 -0600

    White triangle (again).

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,7 @@
 //-- Project Code
 import {waitForDocument} from './utils/dom';
-import {createHTMLContext} from './webgpu';
+import {createBufferWithData, createHTMLContext, loadShader} from './webgpu';
+import shaderUrl from '../shaders/default.wgsl?url';
 
 /**
  * The application entry point.
@@ -10,6 +11,8 @@ import {createHTMLContext} from './webgpu';
  */
 async function main(): Promise<void> {
     await waitForDocument(globalThis.document);
+
+    //-- Setup WebGPU
     const context = await createHTMLContext({
         adapter: {
             powerPreference: 'high-performance'
@@ -18,10 +21,108 @@ async function main(): Promise<void> {
             alphaMode: 'premultiplied'
         }
     });
-    const {canvas} = context;
+    const {canvas, canvasContext, device} = context;
     canvas.id = 'gameCanvas';
     canvas.classList.add('game-canvas');
     globalThis.document.body.appendChild(canvas);
+
+    //-- Prepare WebGPU
+    const bindGroupLayout = device.createBindGroupLayout({
+        entries: []
+    });
+    const bindGroup = device.createBindGroup({
+        layout: bindGroupLayout,
+        entries: []
+    });
+    const pipelineLayout = device.createPipelineLayout({
+        bindGroupLayouts: [bindGroupLayout]
+    });
+    const shader = await loadShader(context, shaderUrl, {
+        compilationHints: [
+            {
+                entryPoint: 'vertex_main',
+                layout: pipelineLayout
+            },
+            {
+                entryPoint: 'fragment_main',
+                layout: pipelineLayout
+            }
+        ]
+    });
+    const pipeline = await device.createRenderPipelineAsync({
+        layout: pipelineLayout,
+        vertex: {
+            module: shader.native,
+            entryPoint: 'vertex_main',
+            buffers: [
+                {
+                    arrayStride: 12,
+                    attributes: [
+                        {
+                            shaderLocation: 0,
+                            format: 'float32x3',
+                            offset: 0
+                        }
+                    ],
+                    stepMode: 'vertex'
+                }
+            ],
+            constants: {}
+        },
+        fragment: {
+            module: shader.native,
+            entryPoint: 'fragment_main',
+            targets: [
+                {
+                    format: navigator.gpu.getPreferredCanvasFormat()
+                }
+            ],
+            constants: {}
+        },
+        primitive: {
+            topology: 'triangle-list'
+        }
+    });
+    // prettier-ignore
+    const vertexData = new Float32Array([
+        -1, -1, 0,
+         1, -1, 0,
+         0,  1, 0
+    ].map((e) => e / 2));
+    const vertexBuffer = createBufferWithData(
+        context,
+        vertexData,
+        GPUBufferUsage.COPY_DST | GPUBufferUsage.VERTEX
+    );
+    const render = (): void => {
+        canvas.width = canvas.clientWidth;
+        canvas.height = canvas.clientHeight;
+        const commandEncoder = device.createCommandEncoder();
+        const renderPass = commandEncoder.beginRenderPass({
+            colorAttachments: [
+                {
+                    loadOp: 'clear',
+                    storeOp: 'store',
+                    view: canvasContext.getCurrentTexture().createView(),
+                    clearValue: {
+                        r: 100 / 255,
+                        g: 149 / 255,
+                        b: 237 / 255,
+                        a: 1
+                    }
+                }
+            ]
+        });
+        renderPass.setPipeline(pipeline);
+        renderPass.setBindGroup(0, bindGroup);
+        renderPass.setViewport(0, 0, canvas.width, canvas.height, 0, 1);
+        renderPass.setVertexBuffer(0, vertexBuffer.native);
+        renderPass.draw(3);
+        renderPass.end();
+        device.queue.submit([commandEncoder.finish()]);
+        requestAnimationFrame(render);
+    };
+    requestAnimationFrame(render);
 }
 
 main()
