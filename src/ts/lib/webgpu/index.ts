180a724ba98cb48b0284c3d20fc80d4457a95120
G'lek Tarssza <glek@glektarssza.com>
2024-08-01 10:21:09 -0600

    Doing some rethinking.

--- src/ts/lib/webgpu/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './types';
-export * from './context';
-export * from './buffer';
-export * from './shader';
-export * from './pipeline';
--- src/ts/lib/webgpu/types.ts
+++
@@ -1,4 +0,0 @@
-/**
- * A type union of acceptable canvases for rendering to.
- */
-export type Canvas = HTMLCanvasElement | OffscreenCanvas;
