d51f7487e2063484529664422566e031850d3d29
G'lek Tarssza <glek@glektarssza.com>
2024-08-20 09:33:12 -0600

    Setup WebGPU context module.

--- src/ts/lib/webgpu/index.ts
+++ src/ts/lib/webgpu/index.ts
@@ -1,1 +1,11 @@
-export {};
+export type * from './type';
+export {
+    Context,
+    ContextBase,
+    ContextBaseOptions,
+    HTMLContext,
+    OffscreenContext,
+    createContext,
+    createHTMLContext,
+    createOffscreenContext
+} from './context';
