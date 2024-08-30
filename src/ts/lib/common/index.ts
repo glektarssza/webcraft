30cd535d767ab06748d8c1f01e28b9592e44edf7
G'lek Tarssza <glek@glektarssza.com>
2024-08-30 09:38:33 -0600

    Added a distinct type.

--- src/ts/lib/common/index.ts
+++ src/ts/lib/common/index.ts
@@ -1,1 +1,7 @@
-export {};
+/**
+ * A type that is distinct from its base type while still being compatible with
+ * it.
+ *
+ * @typeParam TBase - The base type to make a distinct type from.
+ */
+export type Distinct<TBase> = TBase & {readonly __distinct: unique symbol};
