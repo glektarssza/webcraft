81c9ba1dfde318ba98ea5798256acac7e0e6aabc
G'lek Tarssza <glek@glektarssza.com>
2024-09-06 08:58:57 -0600

    Fixed wrong import alias.

--- packages/common/vite.config.ts
+++ packages/common/vite.config.ts
@@ -36,7 +36,7 @@ const config = defineConfig(({mode}) => {
         },
         test: {
             alias: {
-                '@src': path.resolve(import.meta.dirname, './src/ts/')
+                '@src': path.resolve(import.meta.dirname, './src/')
             },
             browser: {
                 enabled: true,
