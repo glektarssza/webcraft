3ed6d3c72829b98fe113be18800889c145d3ee5f
G'lek Tarssza <glek@glektarssza.com>
2025-02-05 18:37:46 +0000

    Always produce coverage but only in text form.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
     test: {
         coverage: {
+            enabled: true,
             all: true,
             provider: 'istanbul',
-            reporter: ['text', 'html']
+            reporter: ['text']
         },
         passWithNoTests: true,
         reporters: 'default'
