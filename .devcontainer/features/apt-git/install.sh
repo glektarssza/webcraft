d8e2ddb5f2382818365e6d6fd4ad20ae45c7f2cc
G'lek Tarssza <glek@glektarssza.com>
2025-03-31 09:17:45 -0600

    Removed dev container files.

--- .devcontainer/features/apt-git/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Import our function library.
-. ./functions.sh;
-
-# Install or upgrade git.
-apt_install_or_upgrade git;
--- .devcontainer/features/apt-procps/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Import our function library.
-. ./functions.sh;
-
-# Install or upgrade ca-certificates.
-apt_install_or_upgrade procps;
