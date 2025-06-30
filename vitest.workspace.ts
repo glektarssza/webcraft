1f7602d2998309e127dd90c377775c63e19d8406
G'lek Tarssza <glek@glektarssza.com>
2025-06-30 09:53:39 -0600

    Move to new Vitest config format (again).

--- vitest.workspace.ts
+++
@@ -1,7 +0,0 @@
-import {defineWorkspace} from 'vitest/config';
-
-export default defineWorkspace([
-    './app/vite.config.ts',
-    './packages/common/vite.config.ts',
-    './packages/logging/vite.config.ts'
-]);
